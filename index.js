fa779be1719eb14b02868a27357378c175852927
rofiatmuritala <rofiatmuritalaa@gmail.com>
2024-03-19 15:11:35 +0000

    Commt

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors"
 import todosRoutes from "./routes/todos.routes.js"
+import dotenv from "dotenv"
+
+
+dotenv.config({ path: [".env.local"] });
 
 // create express app
 const app = express();
@@ -14,8 +18,9 @@ app.use(cors());
 //Use route
 
 app.use(todosRoutes)
+const PORT = process.env.PORT
 
 // Listen for incoming request
-app.listen(4000, () => {
-    console.log("Express app is running!")
+app.listen(PORT, () => {
+    console.log("Express app is running ")
 })
